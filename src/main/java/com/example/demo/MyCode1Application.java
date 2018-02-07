dbeef30d8295841cfee4cbbaed70e15e4c3df739
jianlucky <jianlucky@192.168.19.21>
2018-02-07 12:47:49 +0800

    firstCommit

---
+++ src/main/java/com/example/demo/MyCode1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCode1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCode1Application.class, args);
+	}
+}
