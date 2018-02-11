bc3dd1d1a39bad764c78674ba005d76ab888b755
jianlucky <jianlucky@192.168.19.21>
2018-02-11 15:17:47 +0800

    对数据源上下文支持

--- src/main/java/com/example/config/dynamic/datasource/MultDataSourceContextHolder.java
+++ src/main/java/com/example/config/dynamic/datasource/MultDataSourceContextHolder.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class DataSourceContextHolder {
+public class MultDataSourceContextHolder {
 
 	/*
      * 当使用ThreadLocal维护变量时，ThreadLocal为每个使用该变量的线程提供独立的变量副本，
      * 所以每一个线程都可以独立地改变自己的副本，而不会影响其它线程所对应的副本。
      */
-    private static final ThreadLocal<LinkedList<String>> contextHolder = new ThreadLocal<LinkedList<String>>();
+    private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
     /*
      * 管理所有的数据源id;
      * 主要是为了判断数据源是否存在;
@@ -22,28 +22,13 @@ public class DataSourceContextHolder {
      * @param dataSourceType
      */
     public static void setDataSourceType(String dataSourceType) {
-       LinkedList<String> dsNames=contextHolder.get();
-       if(dsNames==null) {
-    	   		contextHolder.set(new LinkedList<>());
-       }
-       contextHolder.get().addFirst(dataSourceType);
+       contextHolder.set(dataSourceType);
     }
-    
     public static String getDataSourceType() {
-    		LinkedList<String> dsNames=contextHolder.get();
-    		if(dsNames!=null&&dsNames.getFirst()!=null) {
-    			return dsNames.getFirst();
-    		}
-       return null;
+       return contextHolder.get();
     }
     public static void clearDataSourceType() {
-       LinkedList<String> dsNames=contextHolder.get();
-       if(dsNames==null||dsNames.size()==1) {
-           contextHolder.remove();
-       }else {
-    	   	   dsNames.removeFirst();
-       }
-
+       contextHolder.remove();
     }
     /**
      * 判断指定DataSrouce当前是否存在
