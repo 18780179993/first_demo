eb293657e5d6c79cd027e0aa44a9320d0a1e3f6e
Administrator <Administrator@192.168.1.103>
2018-07-10 23:15:27 +0800

    test

--- src/test/java/com/example/demo/MyCode1ApplicationTests.java
+++ src/test/java/com/example/demo/MyCode1ApplicationTests.java
@@ -1,52 +1,52 @@
 package com.example.demo;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.concurrent.locks.Lock;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class MyCode1ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
-	public static void main(String[] args) {
-		
-	 new Thread("t1"){
-		 public void run() {
-			 int i=0;
-			 while (true) {
-				i++;
-				try {
-					Thread.sleep(500l);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-		 };
-	 }.start();
-	 new Thread("t2"){
-		 public void run() {
-			 int j=0;
-			 while (true) {
-				j++;
-				try {
-					Thread.sleep(500l);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-		 };
-	 }.start();
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//	public static void main(String[] args) {
+//		 final Object lock=new Object();
+//		
+//	 new Thread("t1"){
+//		 public void run() {
+//			 synchronized (lock) {
+//				 System.out.println("t1");
+//				 try {
+//					lock.wait();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//				System.out.println("t1_end");
+//			}
+//			
+//		 };
+//	 }.start();
+//	 new Thread("t2"){
+//		 public void run() {
+//			 synchronized (lock) {
+//				 System.out.println("t2");
+//				 try {
+//					lock.wait();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//				System.out.println("t2_end");
+//			}
+//			
+//		 };
+//	 }.start();
+//	}
 }
